3855ff38b1e17d04c3662aa33fd2a1cdd5b796ae
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2024-09-17 13:15:29 -0400

    Update loader.js

--- 02/loader.js
+++ 02/loader.js
@@ -16,7 +16,7 @@ function download() {
   // create JSON object
   var out = {};
   out['cubes'] = ALL_CUBES;
-  out['camera'] = r.camera.view;
+  out['camera'] = CAMERAS; //r.camera.view;
 
   // from https://stackoverflow.com/a/30800715
   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(out));
@@ -56,11 +56,19 @@ function upload(scene) {
     }
 
     // restore camera
-    r.camera.view = new Float32Array(Object.values(loaded['camera']));
+    r.camera.view = new Float32Array(Object.values(loaded['camera'][0]));
+
+    CAMERAS = [];
+    for (var cam in loaded['camera']) {
+
+      cam = loaded['camera'][cam];
+      CAMERAS.push(new Float32Array(Object.values(cam)));
+
+    }
+
 
 
   };
   req.send(null);
   
 }
-
